e66d2404b4f2f0e9dbd069615b5255da7d84233d
SailorGa <ljia789@foxmail.com>
2023-11-29 19:38:20 +0800

    chore：Keychain优化

--- VCSSDK/Common/Constants/FWKeychain.m
+++ VCSSDK/Common/Constants/FWKeychain.m
@@ -8,10 +8,10 @@
 
 #import "FWKeychain.h"
 
-//#pragma mark - VCSSDK Appid
-//NSString * const VCSSDKAPPID = <# 替换成平台分配的Appid #>
-//#pragma mark - VCSSDK AppKey
-//NSString * const VCSSDKAPPKEY = <# 替换成平台分配的AppKey #>
+#pragma mark - VCSSDK Appid
+NSString * const VCSSDKAPPID = <# 替换成平台分配的Appid #>
+#pragma mark - VCSSDK AppKey
+NSString * const VCSSDKAPPKEY = <# 替换成平台分配的AppKey #>
 #pragma mark - VCSSDK Signature
 NSString * const VCSSDKSIGNATURE = @"";
 
